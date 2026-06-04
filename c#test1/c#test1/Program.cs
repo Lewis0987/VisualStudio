d7cc2960bebbb4c2259322a988849aa969b5d4e2
Water <water.lin@mail.etica-inc.com>
2026-06-04 15:57:51 +0800

    Initial commit

---
+++ c#test1/c#test1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace C_test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello World");
+            Debug.WriteLine("這是第二行訊息");
+            Debug.WriteLine("Visual Studio 輸出測試");
+        }
+    }
+}
